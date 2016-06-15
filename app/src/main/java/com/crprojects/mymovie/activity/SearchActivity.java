0e36f1d9ab5fabde581ef9a9fbe1148372093cf7
arjunpratapr <arjunpratapr@gmail.com>
2016-06-15 12:09:45 +0530

    Initial commit

---
+++ app/src/main/java/com/crprojects/mymovie/activity/SearchActivity.java
@@ -0,0 +1,253 @@
+package com.crprojects.mymovie.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.crprojects.mymovie.R;
+import com.crprojects.mymovie.omdbfiles.RetrofitLoader;
+import com.crprojects.mymovie.omdbfiles.SearchService;
+import com.crprojects.mymovie.utils.CommonUtils;
+
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity
+
+        implements LoaderManager.LoaderCallbacks<SearchService.ResultWithDetail>{
+
+
+    private Button mSearchButton;
+    private EditText mSearchEditText;
+    private RecyclerView mMovieListRecyclerView;
+    private MovieRecyclerViewAdapter mMovieAdapter;
+    private String mMovieTitle;
+    private ProgressBar mProgressBar;
+
+    private static final int LOADER_ID = 1;
+
+    private static final String LOG_TAG = "SearchActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        mSearchEditText = (EditText) findViewById(R.id.search_edittext);
+        // set action for pressing search button on keyboard
+        mSearchEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || (event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER))) {
+                    startSearch();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+        mSearchButton = (Button) findViewById(R.id.search_button);
+        mMovieListRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSearch();
+            }
+        });
+        mMovieAdapter = new MovieRecyclerViewAdapter(null);
+        mMovieListRecyclerView.setAdapter(mMovieAdapter);
+        // First param is number of columns and second param is orientation i.e Vertical or Horizontal
+        StaggeredGridLayoutManager gridLayoutManager =
+                new StaggeredGridLayoutManager(getResources().getInteger(R.integer.grid_column_count), StaggeredGridLayoutManager.VERTICAL);
+        mMovieListRecyclerView.setItemAnimator(null);
+        // Attach the layout manager to the recycler view
+        mMovieListRecyclerView.setLayoutManager(gridLayoutManager);
+        getSupportLoaderManager().enableDebugLogging(true);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_spinner);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("mMovieTitle", mMovieTitle);
+        outState.putInt("progress_visibility",mProgressBar.getVisibility());
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+        int progress_visibility= savedInstanceState.getInt("progress_visibility");
+        // if the progressBar was visible before orientation-change
+        if(progress_visibility == View.VISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+        // init the loader, so that the onLoadFinished is called
+        mMovieTitle = savedInstanceState.getString("mMovieTitle");
+        if (mMovieTitle != null) {
+            Bundle args = new Bundle();
+            args.putString("movieTitle", mMovieTitle);
+            getSupportLoaderManager().initLoader(LOADER_ID, args, this);
+        }
+    }
+
+    @Override
+    public Loader<SearchService.ResultWithDetail> onCreateLoader(int id, Bundle args) {
+        return new RetrofitLoader(SearchActivity.this, args.getString("movieTitle"));
+    }
+
+    @Override
+    public void onLoadFinished(Loader<SearchService.ResultWithDetail> loader, SearchService.ResultWithDetail resultWithDetail) {
+        mProgressBar.setVisibility(View.GONE);
+        mMovieListRecyclerView.setVisibility(View.VISIBLE);
+        if(resultWithDetail.getResponse().equals("True")) {
+            mMovieAdapter.swapData(resultWithDetail.getMovieDetailList());
+        } else {
+            Snackbar.make(mMovieListRecyclerView,
+                    getResources().getString(R.string.snackbar_title_not_found), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<SearchService.ResultWithDetail> loader) {
+        mMovieAdapter.swapData(null);
+    }
+
+    public class MovieRecyclerViewAdapter
+            extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder> {
+
+        private List<SearchService.Detail> mValues;
+
+        public MovieRecyclerViewAdapter(List<SearchService.Detail> items) {
+            mValues = items;
+        }
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_movie, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+
+            final SearchService.Detail detail = mValues.get(position);
+            final String title = detail.Title;
+            final String imdbId = detail.imdbID;
+            final String director = detail.Director;
+            final String year = detail.Year;
+            holder.mDirectorView.setText(director);
+            holder.mTitleView.setText(title);
+            holder.mYearView.setText(year);
+
+            final String imageUrl;
+            if (! detail.Poster.equals("N/A")) {
+                imageUrl = detail.Poster;
+            } else {
+                // default image if there is no poster available
+                imageUrl = getResources().getString(R.string.default_poster);
+            }
+            holder.mThumbImageView.layout(0, 0, 0, 0); // invalidate the width so that glide wont use that dimension
+            Glide.with(SearchActivity.this).load(imageUrl).into(holder.mThumbImageView);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(SearchActivity.this, DetailActivity.class);
+                    // Pass data object in the bundle and populate details activity.
+                    intent.putExtra(DetailActivity.MOVIE_DETAIL, detail);
+                    intent.putExtra(DetailActivity.IMAGE_URL, imageUrl);
+
+                    ActivityOptionsCompat options = ActivityOptionsCompat.
+                            makeSceneTransitionAnimation(SearchActivity.this,
+                                    holder.mThumbImageView, "poster");
+                    startActivity(intent, options.toBundle());
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            if(mValues == null) {
+                return 0;
+            }
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mTitleView;
+            public final TextView mYearView;
+            public final TextView mDirectorView;
+            public final ImageView mThumbImageView;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mTitleView = (TextView) view.findViewById(R.id.movie_title);
+                mYearView = (TextView) view.findViewById(R.id.movie_year);
+                mThumbImageView = (ImageView) view.findViewById(R.id.thumbnail);
+                mDirectorView = (TextView) view.findViewById(R.id.movie_director);
+            }
+
+        }
+
+        @Override
+        public void onViewRecycled(ViewHolder holder) {
+            super.onViewRecycled(holder);
+            Glide.clear(holder.mThumbImageView);
+        }
+
+        public void swapData(List<SearchService.Detail> items) {
+            if(items != null) {
+                mValues = items;
+                notifyDataSetChanged();
+
+            } else {
+                mValues = null;
+            }
+        }
+    }
+
+    private void startSearch() {
+        if(CommonUtils.isNetworkAvailable(getApplicationContext())) {
+            CommonUtils.hideSoftKeyboard(SearchActivity.this);
+            String movieTitle = mSearchEditText.getText().toString().trim();
+            if (!movieTitle.isEmpty()) {
+                Bundle args = new Bundle();
+                args.putString("movieTitle", movieTitle);
+                getSupportLoaderManager().restartLoader(LOADER_ID, args, this);
+                mMovieTitle = movieTitle;
+                mProgressBar.setVisibility(View.VISIBLE);
+                mMovieListRecyclerView.setVisibility(View.GONE);
+            }
+            else
+                Snackbar.make(mMovieListRecyclerView,
+                        getResources().getString(R.string.snackbar_title_empty),
+                        Snackbar.LENGTH_LONG).show();
+        } else {
+            Snackbar.make(mMovieListRecyclerView,
+                    getResources().getString(R.string.network_not_available),
+                    Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+
+}
